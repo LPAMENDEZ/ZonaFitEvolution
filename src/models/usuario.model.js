a1eb5d54221407c62fea6062d494d6d7e027902f
LPAMENDEZ <panessoliliana785@gmail.com>
2026-09-07 10:21:32 -0500

    feat: implementar validaciones en usuario.service

--- src/models/usuario.model.js
+++ src/models/usuario.model.js
@@ -1,1 +1,23 @@
-. 
+class Usuario {
+  constructor({ id, nombre, correo, contrasena, rol_id, fecha_creacion }) {
+    this.id = id;
+    this.nombre = nombre;
+    this.correo = correo;
+    this.contrasena = contrasena;
+    this.rol_id = rol_id;
+    this.fecha_creacion = fecha_creacion;
+  }
+
+  // Nunca exponer la contraseña al convertir a JSON (respuestas de API)
+  toJSON() {
+    return {
+      id: this.id,
+      nombre: this.nombre,
+      correo: this.correo,
+      rol_id: this.rol_id,
+      fecha_creacion: this.fecha_creacion
+    };
+  }
+}
+
+module.exports = Usuario;
